a446c84cb2b9cceb2c4a071a25dff116af3feb40
Hamed Asharfi <Ashrafi@outlook.com.au>
2022-08-04 07:01:54 +0000

    (added) hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-4593047fe6a3327b.d
@@ -0,0 +1,5 @@
+/workspace/TheBook/hello_cargo/target/debug/deps/hello_cargo-4593047fe6a3327b.rmeta: src/main.rs
+
+/workspace/TheBook/hello_cargo/target/debug/deps/hello_cargo-4593047fe6a3327b.d: src/main.rs
+
+src/main.rs:
